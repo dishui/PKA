651c30c404222918bd37478e72527f59dedcc71e
dishui <onejosefina@gmail.com>
2025-09-25 13:07:52 -0700

    import the relevant packages

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,7 @@
+from fastapi import FastAPI
+from app.api.router import api_router
+from app.core.config import settings
+
 def create_application() -> FastAPI: 
     """
     Create and configurea the FastAPI application.
